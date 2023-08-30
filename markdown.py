68f2f633f336115f371fb2c4e06e1f0906197a9f
Pan Huiming <panhuiming@sjtu.edu.cn>
2023-08-30 16:23:20 +0800

    Format the file

--- markdown.py
+++ markdown.py
@@ -1,13 +1,14 @@
-import os
-
-
 def word_markdown(word):
-    number_trans=len(word.trans_list)
-    text=text=f"\n> [!WORD]+ {word.spelling}    {word.phonetic}    {word.rank}\n"
-    for order,trans in enumerate(word.trans_list):
-        text+=f"> {order+1}. {trans['property']} {trans['meaning']}\n"
+    """
+    Form the markdown text in Obsidian and return text.
+    :param word: get_data.Word
+    :return: string, Obsidian text
+    """
+    text = f"\n> [!WORD]+ {word.spelling}    {word.phonetic}    {word.rank}\n"
+    for order, trans in enumerate(word.trans_list):
+        text += f"> {order + 1}. {trans['property']} {trans['meaning']}\n"
         if trans["example_list"]:
             for example in trans["example_list"]:
-                text+=f"> \te.g. {example['example_EN']} {example['example_CN']}\n"
-    text+="---\n"
+                text += f"> \te.g. {example['example_EN']} {example['example_CN']}\n"
+    text += "---\n"
     return text
