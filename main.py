56c411ba8d63103026aa9a79da0788de6c403ade
Pan Huiming <panhuiming@sjtu.edu.cn>
2023-08-31 17:07:14 +0800

    Structure

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ with open('info.json') as json_file:
     obsidian_dir = dir_info["obsidian_dir"]
     file_dir = dir_info["English_dir"]
 filename = f"draft English {date}.md"
-
 path = os.path.join(obsidian_dir, file_dir, filename)
-words = read.read_words("file", path="test\\test.txt", filetype="zotero")
+
+word_source = "clipBoard"
+words = read.read_words("file", path="test\\test.txt", filetype=word_source)
 
 
 with open(path, encoding="utf-8", mode="w+") as f:
