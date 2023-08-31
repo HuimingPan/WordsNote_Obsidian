64d9326ec3ce934bbedc00da58802316ce2bb518
Pan Huiming <panhuiming@sjtu.edu.cn>
2023-08-31 17:06:58 +0800

    Add `clipBoard` input method.

--- read.py
+++ read.py
@@ -14,15 +14,16 @@ def read_words(source: str, **par):
 
 def read_words_from_file(path: str, filetype: str):
     """
-    Obtain the words to be looked up from a txt file which contains the text copied from zotero.
+    Obtain the words to be looked up from a txt file which contains the text copied from zotero/foxit.
     :param path: The path where the file locates.
     :return: A list containing words, for example ["word","vocabulary"].
     """
-    if filetype=="zotero":
-        reg = '“([a-z]+)”'
-    elif filetype=="foxitPDF":
-        reg = r'([a-z]+)\n'
-    reg = re.compile(reg)
+    regs = {"zotero": r'“([a-z]+)”',
+            "foxitPDF":r'([a-z]+)\n',
+            "clipBoard": r'([a-z]+)'}
+    reg =re.compile(regs[filetype])
+    if filetype == "clipBoard":
+        return reg.findall(input("Paste: "))
     with open(path, encoding="utf-8") as f:
         # 正则表达式的使用
         original_text = f.read()
